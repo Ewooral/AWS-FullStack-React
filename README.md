20b4a7424b081fe33bf2d21099e682f8f5ade2ae
Ewooral <boahen.cos@gmail.com>
2022-12-26 21:11:33 +0000

    ...

---
+++ README.md
@@ -0,0 +1,4 @@
+one click to deploy on AWS amplify
+<a href="https://console.aws.amazon.com/amplify/home#/deploy?repo=https://github.com/username/repository">
+        <img src="https://oneclick.amplifyapp.com/button.svg" alt="Deploy to Amplify Console" />
+</a>
--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,8 @@ function App() {
 
   return (
     <div className="App">
-      Hi, How are you doing?
-      <a href="https://console.aws.amazon.com/amplify/home#/deploy?repo=https://github.com/username/repository">
-        <img src="https://oneclick.amplifyapp.com/button.svg" alt="Deploy to Amplify Console" />
-      </a>
+      <a href="#">Hi, How are you doing?</a>
+
     </div>
   )
 }
